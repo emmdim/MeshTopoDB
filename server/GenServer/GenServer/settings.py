d9b7b9fbe0a801f3dd0385a6ea978ac447f83496
manos <savemanos@gmail.com>
2013-05-25 20:51:12 +0200

    Added poxweb which is actually the Pox source. Added Flow from poxweb to admin interface Added Switch and Node from topologic to admin interface Created l2_learning_django.py to use the ready flows

--- server/GenServer/GenServer/settings.py
+++ server/GenServer/GenServer/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'topologic',
+    'poxweb',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
---
+++ server/GenServer/topologic/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from topologic.models import Node
+from topologic.models import Switch
+
+admin.site.register(Node)
+admin.site.register(Switch)
